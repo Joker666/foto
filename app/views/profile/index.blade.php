60b8a4ff929d7ffc13c174eed5201de9c590c7a7
Hasan Rafi <mr.k779@outlook.com>
2014-05-23 12:33:50 +0600

    Sidebar

--- app/views/profile/index.blade.php
+++ app/views/profile/index.blade.php
@@ -17,8 +17,27 @@
     </a>
 </nav>
 <div class="sb-slidebar sb-left">
-    <!-- Your left Slidebar content. -->
-    <p>hello</p>
+    <ul class="sb-menu">
+        <li><a href="">Home</a></li>
+        <li><a href="">ABCD</a></li>
+        <li><a href="">DEFG</a></li>
+        <li><a href="">FRGT</a></li>
+        <li><a href="">WWWW</a></li>
+        <li><a href="#" class="sb-toggle-submenu">Help &amp; Issues <span class="sb-caret"></span></a>
+            <ul class="sb-submenu">
+                <li><a href="">BJHJB</a></li>
+                <li><a href="">MKJMKM</a></li>
+                <li><a href="">SDDSD</a></li>
+                <li><a href="">DDSSDSD</a></li>
+            </ul>
+        </li>
+        <li><a href="">Contact</a></li>
+        <li><a class="github" href="">Github</a></li>
+        <li><span class="sb-open-right">About the Author</span></li>
+        <li>
+            <small>foto &copy; 2014 Hasan Rafi</small>
+        </li>
+    </ul>
 </div>
 
 <h1>Profile</h1>
--- public/_/js/script.js
+++ public/_/js/script.js
@@ -12,6 +12,18 @@
             mySlidebars.toggle('right');
             $('div.navbar-left').toggleClass('menu-toggle-box-shadow');
         });
+        // Slidebars Submenus
+        $('.sb-toggle-submenu').off('click').on('click', function() {
+            $submenu = $(this).parent().children('.sb-submenu');
+            $(this).add($submenu).toggleClass('sb-submenu-active'); // Toggle active class.
+
+            if ($submenu.hasClass('sb-submenu-active')) {
+                $submenu.slideDown(200);
+            } else {
+                $submenu.slideUp(200);
+            }
+        });
+
         //$('.sb-toggle-left').click(mySlidebars.close);
     });
 }) (jQuery);
